74a5e6010f63389a89fbcc21c957e755b69405e5
vivek <vivekvarmanizampanam@gmail.com>
2025-12-24 10:33:42 +0530

    ECommerce Backend Completed

--- src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -29,6 +29,7 @@ public class JwtUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
+
     @Value("${spring.app.jwtSecret}")
     private String jwtSecret;
     @Value("${spring.app.jwtExpirationMs}")
